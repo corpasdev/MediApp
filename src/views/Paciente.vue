a619f385344bff2cc475c445195822571deb868b
unknown <brandoncorpas01@gmail.com>
2023-05-15 10:30:21 -0500

    Updates

--- src/views/Paciente.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <h1>Paciente</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur alias nostrum ipsa quam? Laboriosam neque repellendus aliquid dolorum ea commodi beatae
-    </p>
-</template>
